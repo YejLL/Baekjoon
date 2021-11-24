33d750de5b1e10bf4ef91b4213479d1fa485456b
Chloé Y <80348069+YejLL@users.noreply.github.com>
2021-11-24 17:02:01 +0100

    Add files via upload

---
+++ n11022/n11022.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int put_num1, put_num2;
+	int	test_case;
+	int	sum;
+
+	printf("How many times you want to test? : ");
+	scanf("%d", &test_case);
+	for (int x = 1; x <= test_case; x++)
+	{
+		printf("put a value of num1,2! : ");
+		scanf("%d %d", &put_num1, &put_num2);
+		sum = put_num1 + put_num2;
+		printf("Case #%d: %d + %d = %d\n", x, put_num1, put_num2, sum);
+	}
+	return (0);
+}
